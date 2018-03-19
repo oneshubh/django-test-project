82066ae2cef074393b42ffc3ba8cad8a060042d1
oneshubh <oneshubh@gmail.com>
2018-03-20 01:37:37 +0530

    Added static files

--- dummyproject/settings.py
+++ dummyproject/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# POLLS_STATIC_DIR = os.path.join(BASE_DIR,'polls/static/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     POLLS_STATIC_DIR,
+# ]
